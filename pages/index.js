caac4f960bdfb32f8daac2f9cc015da4c93e660d
kuldeepumaraiya <80246040+kuldeepumaraiya@users.noreply.github.com>
2021-03-24 19:42:10 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -34,10 +34,7 @@ function Target({target,radius}) {
 
   return(
     <div className="target-box" style={{
-      left:`${target[0]-2*radius}px`,
-      top:`${target[1]-2*radius}px`,
-      width:`${0*radius}px`,
-      height:`${0*radius}px`
+      
     }}>
       <div className="target-core" style={{
         left:`${radius}px`,
